50f724f7fb9f28adc2dbaa0ec0aee37fa2e047a6
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-02-19 21:51:52 -0300

    ex 2.1: atualização de valor de uma varíavel let

---
+++ topico-2/ex-1.js
@@ -0,0 +1,5 @@
+const nomeJogador = "Ana"
+let pontuacao = 0
+console.log("O jogador " + nomeJogador + " tem " + pontuacao + " pontos.")
+pontuacao += 10
+console.log("O jogador " + nomeJogador + " tem " + pontuacao + " pontos.")

bb2944ca57b0820a37aae0c0d97ebe231300b5dc
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-02-19 21:59:08 -0300

    ex 3.1: alterando de null para valor não nulo

---
+++ topico-3/ex-1.js
@@ -0,0 +1,6 @@
+let enderecoEntrega = null
+let numeroPedido = null
+console.log(enderecoEntrega, numeroPedido)
+enderecoEntrega = "Rua 1"
+numeroPedido = 1
+console.log("O pedido de número " + numeroPedido + " tem o endereço " + enderecoEntrega + " associado.")

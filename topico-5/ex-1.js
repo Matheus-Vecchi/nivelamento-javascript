acb005e4ac0c41b5eed11cd7198211f54a9ab980
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-02-19 22:30:43 -0300

    ex 5.1: criando condicionais

---
+++ topico-5/ex-1.js
@@ -0,0 +1,10 @@
+let nota = 8 // aprovado
+nota = 4 // reprovado
+
+if (nota >= 7) {
+    console.log("Aprovado")
+} else if (5 <= nota <= 6) {
+    console.log("Recuperação")
+} else {
+    console.log("Reprovado")
+}

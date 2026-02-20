39a6f7cf711babf1e5ce27876d386059aaf41ca3
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-02-19 21:54:57 -0300

    ex 2.2: typeof de variáveis

---
+++ topico-2/ex-2.js
@@ -0,0 +1,9 @@
+let nome = "Cléber Gladiador"
+let idade = 40
+let isRetired = true
+let fake
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof isRetired)
+console.log(typeof fake)
